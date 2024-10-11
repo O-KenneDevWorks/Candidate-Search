bcc82725ff8291bcda3daf931c9ec56190991e92
OKenne <79246841+LuciferGreyInc@users.noreply.github.com>
2024-10-11 14:19:14 -0400

    Update API.tsx
    Enabled logging and added rate limit logging

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,7 +1,7 @@
 const searchGithub = async () => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
+    console.log(import.meta.env);
     const response = await fetch(
       `https://api.github.com/users?since=${start}`,
       {
@@ -10,15 +10,16 @@ const searchGithub = async () => {
         },
       }
     );
-    // console.log('Response:', response);
+    console.log('Response:', response);
     const data = await response.json();
+    console.log(data.length);
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
-    // console.log('Data:', data);
+    console.log('Data:', data);
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.log('an error occurred', err);
     return [];
   }
 };
@@ -30,13 +31,15 @@ const searchGithubUser = async (username: string) => {
         Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
       },
     });
+    console.log('Rate Limit Remaining:', response.headers.get('X-RateLimit-Remaining')); // Logs remaining requests
+
     const data = await response.json();
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.log('an error occurred', err);
     return {};
   }
 };
